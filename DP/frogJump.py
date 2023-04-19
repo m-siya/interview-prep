389e32fcff53ffed15635a78e819232e37b23dd5
Siya <del.siyamittal@gmail.com>
2023-04-19 10:42:00 +0530

    added extended problem sol

--- DP/frogJump.py
+++ DP/frogJump.py
@@ -1,3 +1,9 @@
+# Given a number of stairs and a frog, the frog wants to climb from the 0th stair to the (N-1)th stair. 
+# At a time the frog can climb either one or two steps. A height[N] array is also given. Whenever the frog
+# jumps from a stair i to stair j, the energy consumed in the jump is abs(height[i]- height[j]), where abs() 
+# means the absolute difference.
+# We need to return the minimum energy that can be used by the frog to jump from stair 0 to stair N-1.
+
 class Solution:
     #Top Down/ Memoization Method
     def minimumEnergy(self, height, n):
@@ -68,3 +74,40 @@ class Solution:
 s = Solution()
 print(s.minimumEnergy_SO([10, 20, 30], 3))
 print(s.minimumEnergy([10, 20, 30], 3))
+
+
+## VARIABLE FROG JUMP   
+# There are n stones and an array of heights and a frog is standing at stone 1 and can jump 
+# to one of the following: Stone i+1, i+2, ... i+k stone and cost will be [hi-hj] is incurred,
+# where j is the stone to land on. Find the minimum possible total cost incurred before the frog 
+# reaches Stone N.
+
+class SolutionV:
+    #time complexity : O(N * K) -> an additional k factor for the for loop in function body
+    #space complexity : O(N)
+    def minimumEnergy(self, height, n, k):
+        
+        #f(i) -> min cost incurred to jump to ith stone
+        dp = [-1] * n
+
+        def f(i):
+            if i == 0:
+                return 0
+
+            if dp[i] != -1:
+                return dp[i]
+                   
+            minCost = float('inf')
+            for j in range(i - 1, i - k - 1, -1):
+                if j >= 0 and (i - j) >= 0:
+                    currCost = abs(height[i] - height[j]) + f(j)
+                    minCost = min(minCost, currCost)
+            dp[i] = minCost    
+            return dp[i]
+            
+        
+        return f(n - 1)
+                
+s = SolutionV()
+#print(s.minimumEnergy_SO([10, 20, 30], 3))
+print(s.minimumEnergy([10, 30, 40, 50, 20], 5, 3))                
