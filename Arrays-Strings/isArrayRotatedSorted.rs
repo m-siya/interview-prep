62201580ce1dd881f147d93e2605b2805e8da51a
Siya <del.siyamittal@gmail.com>
2023-06-08 13:05:21 +0530

    added sol

---
+++ Arrays-Strings/isArrayRotatedSorted.rs
@@ -0,0 +1,24 @@
+// ### CHECK IF ARRAY IS SORTED AND ROTATED
+
+// Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated 
+// some number of positions (including zero). Otherwise, return false.
+
+// There may be duplicates in the original array.
+
+// Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], 
+// where % is the modulo operation.
+
+// https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+
+impl Solution {
+    pub fn check(nums: Vec<i32>) -> bool {
+        let mut turning_points = 0;
+        for i in 0..nums.len() {
+            if nums[i] > nums[(i + 1) % nums.len()] {
+                turning_points += 1;
+            }
+        }
+
+        turning_points <= 1
+    }
+}
