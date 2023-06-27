e43cd8de7c03a8261b5dad908b831a50cef5165b
Siya <del.siyamittal@gmail.com>
2023-06-27 12:29:05 +0530

    div by k and continous subarray ans

--- Arrays-Strings/hashing/subarraySumEqualsK.cpp
+++ Arrays-Strings/hashing/subarraySumEqualsK.cpp
@@ -26,9 +26,88 @@ public:
             map[sum] ++;
         }
 
-        
-
-
         return result;
     }
 };
+
+// ### SUBARRAY SUMS DIVISIBLE BY K
+// Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+// A subarray is a contiguous part of an array.
+
+// https://leetcode.com/problems/subarray-sums-divisible-by-k/description/
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map <int, int> map;
+
+        int sum = 0, result = 0;
+        map[sum] = 1;
+
+        for (int num: nums) {
+            sum = (sum + num % k + k) % k;
+            result += map[sum];
+            map[sum]++;
+        }
+
+        return result;
+        
+    }
+};
+
+// ### CONTINUOUS SUBARRAY SUM
+
+// Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+// A good subarray is a subarray where:
+
+// its length is at least two, and
+// the sum of the elements of the subarray is a multiple of k.
+// Note that:
+
+// A subarray is a contiguous part of the array.
+// An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+ 
+// https://leetcode.com/problems/continuous-subarray-sum/description/
+
+// method -> 
+// sum % k = 0
+// => (p[j] - p[i]) % k = 0
+// => p[j] % k - p[i] % k = 0
+// => p[j] % k = p[i] % k,  where j - i >= 1
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        // map = {prefix_sum % k : index of first occurence}
+
+        if (nums.size() < 2) return false;
+
+        int sum = 0;
+        map[sum] = -1;
+      //  map[sum] = 1;
+        
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum = (sum + nums[i]) % k;
+           // cout << map[sum] << endl;
+           if (map.find(sum) != map.end()){
+               //sum already exists
+               if (i - map[sum] > 1) return true;
+           }
+           else {
+               map[sum] = i;
+           }
+        }
+
+        // for (auto pair: map) {
+        //     cout << pair.first << ": " << pair.second << endl;
+        // }
+
+        return false;      
+    }
+};
+
+
