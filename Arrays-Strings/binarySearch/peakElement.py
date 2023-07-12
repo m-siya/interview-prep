971d0ecfc513fd36aa11eba0e205bee433e77275
Siya <del.siyamittal@gmail.com>
2023-07-12 13:57:54 +0530

    add related ques

--- Arrays-Strings/binarySearch/peakElement.py
+++ Arrays-Strings/binarySearch/peakElement.py
@@ -37,3 +37,38 @@ class Solution:
 
         
         return len(nums) - 1
+
+
+# ### PEAK INDEX IN MOUNTAIN ARRAY
+
+# An array arr a mountain if the following properties hold:
+
+# arr.length >= 3
+# There exists some i with 0 < i < arr.length - 1 such that:
+# arr[0] < arr[1] < ... < arr[i - 1] < arr[i] 
+# arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+# Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+# You must solve it in O(log(arr.length)) time complexity.
+
+# https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+ 
+# time complexity - O(logn), space - O(1)
+
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        low, high = 0, len(arr) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+           # print(low, mid, high)
+
+            if (mid - 1 >= 0 and mid + 1 < len(arr)) and arr[mid - 1] > arr[mid] > arr[mid + 1]:
+                high = mid 
+            elif (mid - 1 >= 0 and mid + 1 < len(arr)) and arr[mid + 1] > arr[mid] > arr[mid - 1]:
+                low = mid 
+            else:
+                return mid
+
+    
+
